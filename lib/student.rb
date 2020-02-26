3b3de983d35ce2e22c3ce821ff74779ef6c63b19
Damely Tineo <damely.tineo07@gmail.com>
2020-02-26 04:42:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -122,11 +122,11 @@ class Student
     end.first
   end
   
-  def self.all_students_in_grade_X(grade)
+  def self.all_students_in_grade_X(gradex)
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = grade
+      WHERE grade = gradxe
     SQL
   
     DB[:conn].execute(sql).map do |row|
